913f73c1fa70f9d7c7cf13eaaac3ed93894ef5b8
waimea-nawaite <nawaite@waimea.school.nz>
2026-04-22 15:10:18 +1200

    map is beautiful big and i get lost

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,7 +20,7 @@ class Location(
     val description: String,
     val distanceToStartPod: Int,
 ) {
-    var visited: Boolean = false
+    var hasPda: Boolean = false
 
     fun info(): String {
         val infoText = "$podName is at $description ${distanceToStartPod}M"
@@ -39,50 +39,91 @@ class Game() {
     var name = "Ned"
     var score = 0
 
+    fun scorePdas(pdas: Int) {
+        score += pdas
+    }
+
+    fun maxScoreReached(): Boolean {
+        return score >= 4
+    }
+
     val lifepods = mutableListOf<Location>()
     val blocked = Location("BLOCKED", "", 0)
+    val openOcean = Location("OpenOcean", "Nothing but water here", 0)
 
     var currentPodIndex: Int    // Index into the list of the player's location
 
     init {
         val lifepod5 = Location("Lifepod 5", "Safe shallows the only floating pod that survived", 0)
-        val lifepod3 = Location("Lifepod 3", "Kelp Forest lush green kelp is mostly safe", 250)
         val lifepod17 = Location("Lifepod 17", "Open Grassy Plateaus, good visibility", 350)
         val lifepod6 = Location("Lifepod 6", "Grassy Plateaus, slightly deeper and more open", 450)
         val lifepod13 = Location("Lifepod 13", "Mushroom Forest, unique and visually distinct", 650)
         val lifepod7 = Location("Lifepod 7", "Crag Field, rugged terrain with more tension", 750)
         val lifepod19 = Location("Lifepod 19", "Sparse Reef, darker and more isolated", 850)
         val lifepod12 = Location("Lifepod 12", "Bulb Zone, alien environment", 950)
+        val lifepod3 = Location("Lifepod 3", "Far but seems safe and calm...", 1100)
         val lifepod2 = Location("Lifepod 2", "Blood Kelp Zone, furthest and very dangerous", 1200)
 
         lifepods.add(lifepod5)
-        lifepods.add(lifepod3)
+        lifepods.add(openOcean)
+        lifepods.add(blocked)
+        lifepods.add(blocked)
+
         lifepods.add(blocked)
         lifepods.add(lifepod17)
-        lifepods.add(blocked)
-        lifepods.add(lifepod7)
-        lifepods.add(lifepod13)
+        lifepods.add(openOcean)
         lifepods.add(lifepod6)
+
+        lifepods.add(openOcean)
+        lifepods.add(openOcean)
+        lifepods.add(blocked)
+        lifepods.add(blocked)
+
+        lifepods.add(lifepod13)
+        lifepods.add(blocked)
+        lifepods.add(openOcean)
+        lifepods.add(lifepod7)
+
+        lifepods.add(openOcean)
+        lifepods.add(openOcean)
         lifepods.add(lifepod19)
+        lifepods.add(blocked)
+
+        lifepods.add(blocked)
         lifepods.add(lifepod12)
         lifepods.add(blocked)
         lifepods.add(lifepod2)
 
+        lifepods.add(openOcean)
+        lifepods.add(openOcean)
+        lifepods.add(lifepod3)
+        lifepods.add(openOcean)
+
+        lifepods
+            .filter { it != blocked && it != lifepod5 }
+            .shuffled()
+            .take(4)
+            .forEach {
+                it.hasPda = true
+            }
+
         currentPodIndex = 0
+
     }
 
     fun goNorth() {
-        if (currentPodIndex - 3 >= 0) {
-            currentPodIndex -= 3
+        if (canGoNorth()) {
+            currentPodIndex -= 4
+            checkForPda()
         }
     }
 
     fun canGoNorth(): Boolean {
         // Are we at top edge of map?
-        if (currentPodIndex - 3 < 0) return false
+        if (currentPodIndex - 4 < 0) return false
 
         // See what is to the north
-        val northPod = lifepods[currentPodIndex - 3]
+        val northPod = lifepods[currentPodIndex - 4]
         if (northPod == blocked) return false
 
         // Not of edge, and not blocked
@@ -90,13 +131,14 @@ class Game() {
     }
 
     fun goEast() {
-        if (currentPodIndex % 3 != 2) {
+        if (canGoEast()) {
             currentPodIndex++
+            checkForPda()
         }
     }
 
     fun canGoEast(): Boolean {
-        if (currentPodIndex % 3 == 2) return false
+        if (currentPodIndex % 4 == 3) return false
 
         val eastPod = lifepods[currentPodIndex + 1]
         if (eastPod == blocked) return false
@@ -105,30 +147,32 @@ class Game() {
     }
 
     fun goSouth() {
-        if (currentPodIndex + 3 <= 11) {
-            currentPodIndex += 3
+        if (canGoSouth()) {
+            currentPodIndex += 4
+            checkForPda()
         }
     }
 
     fun canGoSouth(): Boolean {
 
-        if (currentPodIndex + 3 > 11) return false
+        if (currentPodIndex + 4 > 27) return false
 
-        val southPod = lifepods[currentPodIndex + 3]
+        val southPod = lifepods[currentPodIndex + 4]
         if (southPod == blocked) return false
 
         return true
     }
 
     fun goWest() {
-        if (currentPodIndex % 3 != 0) {
+        if (canGoWest()) {
             currentPodIndex--
+            checkForPda()
         }
     }
 
     fun canGoWest(): Boolean {
 
-        if (currentPodIndex % 3 == 0) return false
+        if (currentPodIndex % 4 == 0) return false
 
         val westPod = lifepods[currentPodIndex - 1]
         if (westPod == blocked) return false
@@ -136,16 +180,13 @@ class Game() {
         return true
     }
 
-    fun scorePoints(points: Int) {
-        score += points
-    }
+    fun checkForPda() {
+        val location = lifepods[currentPodIndex]
 
-    fun resetScore() {
-        score = 0
-    }
-
-    fun maxScoreReached(): Boolean {
-        return score >= 10000
+        if (location.hasPda) {
+            location.hasPda = false
+            scorePdas(1)
+        }
     }
 }
 
@@ -163,6 +204,7 @@ class MainWindow(val game: Game) {
     private val lifepodLabel = JLabel()
     private val descriptionLabel = JLabel()
     private val distanceLabel = JLabel()
+    private val allPdasCollectedLabel = JLabel("All PDAS collected return to lifepod 5")
     private val northButton = JButton("North")
     private val eastButton = JButton("East")
     private val southButton = JButton("South")
@@ -186,6 +228,7 @@ class MainWindow(val game: Game) {
         lifepodLabel.setBounds(30, 90, 600, 30)
         descriptionLabel.setBounds(30, 120, 600, 30)
         distanceLabel.setBounds(30, 150, 600, 30)
+        allPdasCollectedLabel.setBounds(30, 300, 600, 30)
         infoButton.setBounds(360, 550, 70, 40)
         northButton.setBounds(110, 460, 90, 40)
         eastButton.setBounds(200, 500, 90, 40)
@@ -198,6 +241,7 @@ class MainWindow(val game: Game) {
         panel.add(lifepodLabel)
         panel.add(descriptionLabel)
         panel.add(distanceLabel)
+        panel.add(allPdasCollectedLabel)
         panel.add(infoButton)
         panel.add(northButton)
         panel.add(eastButton)
@@ -211,6 +255,7 @@ class MainWindow(val game: Game) {
         lifepodLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
         descriptionLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
         distanceLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
+        allPdasCollectedLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
 
         infoButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
     }
@@ -294,16 +339,16 @@ class MainWindow(val game: Game) {
  * @param app the app state object
  */
 class InfoWindow(val owner: MainWindow, val game: Game) {
-    private val dialog = JDialog(owner.frame, "DIALOG TITLE", false)
+    private val dialog = JDialog(owner.frame, "DATA", false)
     private val panel = JPanel().apply { layout = null }
 
     private val infoLabel = JLabel()
-    private val resetButton = JButton("Reset")
+//    private val resetButton = JButton("Reset")
 
     init {
         setupLayout()
         setupStyles()
-        setupActions()
+//        setupActions()
         setupWindow()
         updateUI()
     }
@@ -312,15 +357,15 @@ class InfoWindow(val owner: MainWindow, val game: Game) {
         panel.preferredSize = java.awt.Dimension(240, 180)
 
         infoLabel.setBounds(30, 30, 180, 60)
-        resetButton.setBounds(30, 120, 180, 30)
+//        resetButton.setBounds(30, 120, 180, 30)
 
         panel.add(infoLabel)
-        panel.add(resetButton)
+//        panel.add(resetButton)
     }
 
     private fun setupStyles() {
         infoLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-        resetButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
+//        resetButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
     }
 
     private fun setupWindow() {
@@ -330,20 +375,20 @@ class InfoWindow(val owner: MainWindow, val game: Game) {
         dialog.pack()
     }
 
-    private fun setupActions() {
-        resetButton.addActionListener { handleResetClick() }
-    }
+//    private fun setupActions() {
+//        resetButton.addActionListener { handleResetClick() }
+//    }
 
-    private fun handleResetClick() {
-        game.resetScore()    // Update the app state
-        owner.updateUI()    // Update the UI to reflect this, via the main window
-    }
+//    private fun handleResetClick() {
+//       game.resetScore()    // Update the app state
+//        owner.updateUI()    // Update the UI to reflect this, via the main window
+//    }
 
     fun updateUI() {
         // Use app properties to display state
         infoLabel.text = "<html>User: ${game.name}<br>Score: ${game.score} points"
 
-        resetButton.isEnabled = game.score > 0
+//        resetButton.isEnabled = game.score > 0
     }
 
     fun show() {
